c7c514155d2c6b73f0003cf9a8f3fb00be962894
mohammedyh <mohammedyousufhamid@gmail.com>
2023-06-11 23:00:11 +0100

    Install clerk node sdk

--- server/src/server.js
+++ server/src/server.js
@@ -1,9 +1,14 @@
 import express from "express";
+import "dotenv/config";
+import clerk from "@clerk/clerk-sdk-node";
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.get("/", (_, res) => res.status(200).json({ success: true }));
+
+const userList = await clerk.users.getUserList();
+console.log(userList);
 
 app.listen(PORT, () =>
   console.log(`Server started at http://localhost:${PORT}`)
