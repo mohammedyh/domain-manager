b2ab37d8a023bb78c8bd6012aae5a122789f944b
mohammedyh <mohammedyousufhamid@gmail.com>
2023-06-11 22:59:24 +0100

    Configure clerk and react router

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,35 @@
+import { ClerkProvider, SignIn, SignUp } from '@clerk/clerk-react'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
 import './index.css'
 
+if (!import.meta.env.VITE_REACT_APP_CLERK_PUBLISHABLE_KEY) {
+  throw new Error('Missing publishable key')
+}
+
+const clerkPubKey = import.meta.env.VITE_REACT_APP_CLERK_PUBLISHABLE_KEY
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />
+  },
+  {
+    path: '/auth/signin/',
+    element: <SignIn signUpUrl='/auth/signup' />
+  },
+  {
+    path: '/auth/signup/',
+    element: <SignUp />
+  }
+])
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <ClerkProvider publishableKey={clerkPubKey}>
+      <RouterProvider router={router} />
+    </ClerkProvider>
   </React.StrictMode>,
 )
