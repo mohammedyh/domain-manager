501511d317dfe17d706a4c5825f64b5a7133a2e3
mohammedyh <mohammedyousufhamid@gmail.com>
2023-06-13 17:23:38 +0100

    Remove assets and add clerk logic to app page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useUser } from "@clerk/clerk-react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const { user } = useUser();
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          gap: "0.75rem",
+          alignItems: "center",
+        }}
+      >
+        <img
+          style={{ borderRadius: "50%" }}
+          src={user?.imageUrl}
+          alt="Profile image"
+          width="75"
+          height="75"
+        />
+        <p style={{ fontSize: "18px" }}>
+          {user?.primaryEmailAddress?.emailAddress}
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
