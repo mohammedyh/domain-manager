a0791421c3148b53b820095ecb0ea923fdf2ece1
mohammedyh <mohammedyousufhamid@gmail.com>
2023-06-13 17:24:14 +0100

    Add routing for auth pages

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,35 +1,54 @@
-import { ClerkProvider, SignIn, SignUp } from '@clerk/clerk-react'
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { RouterProvider, createBrowserRouter } from 'react-router-dom'
-import App from './App.jsx'
-import './index.css'
+import {
+  ClerkProvider,
+  SignIn,
+  SignUp,
+  SignedIn,
+  SignedOut,
+} from "@clerk/clerk-react";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import {
+  RouterProvider,
+  createBrowserRouter,
+  Navigate,
+} from "react-router-dom";
+import App from "./App.jsx";
+import "./index.css";
 
-const clerkPubKey = import.meta.env.VITE_REACT_APP_CLERK_PUBLISHABLE_KEY
+const clerkPubKey = import.meta.env.VITE_REACT_APP_CLERK_PUBLISHABLE_KEY;
 
 if (!clerkPubKey) {
-  throw new Error('Missing publishable key')
+  throw new Error("Missing publishable key");
 }
 
 const router = createBrowserRouter([
   {
-    path: '/',
-    element: <App />
+    path: "/",
+    element: (
+      <>
+        <SignedIn>
+          <App />
+        </SignedIn>
+        <SignedOut>
+          <Navigate to="/signin" />
+        </SignedOut>
+      </>
+    ),
   },
   {
-    path: '/auth/signin/',
-    element: <SignIn signUpUrl='/auth/signup' />
+    path: "/signin/",
+    element: <SignIn signUpUrl="/signup" />,
   },
   {
-    path: '/auth/signup/',
-    element: <SignUp />
-  }
-])
+    path: "/signup/",
+    element: <SignUp signInUrl="/signin" />,
+  },
+]);
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ClerkProvider publishableKey={clerkPubKey}>
       <RouterProvider router={router} />
     </ClerkProvider>
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
