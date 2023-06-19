5089fe6e8031bbd3515ce11956ff18332e616e71
mohammedyh <mohammedyousufhamid@gmail.com>
2023-06-19 21:01:33 +0100

    Create API endpoint for adding a domain

--- server/src/server.js
+++ server/src/server.js
@@ -1,14 +1,31 @@
-import express from "express";
+import { Prisma, PrismaClient } from "@prisma/client";
 import "dotenv/config";
-import clerk from "@clerk/clerk-sdk-node";
+import express from "express";
 
+const prisma = new PrismaClient();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.get("/", (_, res) => res.status(200).json({ success: true }));
+app.use(express.json());
 
-const userList = await clerk.users.getUserList();
-console.log(userList);
+// const userList = await clerk.users.getUserList();
+
+app.post("/api/domains/add", async (req, res) => {
+  const { domainName, userId } = req.body;
+
+  if (!domainName) {
+    return res.status(400).json({ message: "Domain name is missing" });
+  }
+
+  const domain = await prisma.domain.create({
+    data: {
+      domainName,
+      user: userId,
+    },
+  });
+
+  return res.json({ message: "Successfully created domain", domain });
+});
 
 app.listen(PORT, () =>
   console.log(`Server started at http://localhost:${PORT}`)
