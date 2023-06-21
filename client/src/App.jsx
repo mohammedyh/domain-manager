5928b86cac5ac4bac56e1a4620b1feefdf5396da
mohammedyh <mohammedyousufhamid@gmail.com>
2023-06-21 23:58:48 +0100

    Implement useSWR for API calls and dynamically render domain data

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,4 @@
-import { UserButton } from "@clerk/clerk-react";
+import { UserButton, useAuth } from "@clerk/clerk-react";
 import {
   Badge,
   Button,
@@ -18,10 +18,21 @@ import {
 import { Box, Clock4, Pencil, ShieldAlert, Trash } from "lucide-react";
 import { useState } from "react";
 import { Toaster } from "sonner";
+import useSWR from "swr";
 import Modal from "./components/Modal";
 
 function App() {
+  const { getToken } = useAuth();
+  const fetcher = async (url) =>
+    fetch(url, {
+      headers: { Authorization: `Bearer ${await getToken()}` },
+    }).then((res) => res.json());
+  const { data, error, isLoading } = useSWR("/api/domains", fetcher);
   const [showModal, setShowModal] = useState(false);
+
+  if (error) return <div>Failed to load</div>;
+  if (isLoading) return <div>Loading...</div>;
+
   return (
     <main className="p-8">
       <Toaster />
@@ -43,7 +54,7 @@ function App() {
             <Icon icon={Box} variant="light" size="xl" color="indigo" />
             <div>
               <Text>Total Domains</Text>
-              <Metric className="mt-2">2</Metric>
+              <Metric className="mt-2">{data.domains.length}</Metric>
             </div>
           </Flex>
         </Card>
@@ -86,38 +97,45 @@ function App() {
           </TableHead>
 
           <TableBody>
-            <TableRow>
-              <TableCell>mohammedcodes.dev</TableCell>
-              <TableCell>05/04/2024</TableCell>
-              <TableCell>05/04/2023</TableCell>
-              <TableCell>12/04/2023</TableCell>
-              <TableCell>
-                <Badge color="green" size="xs">
-                  <span className="font-medium">Active</span>
-                </Badge>
-              </TableCell>
-              <TableCell>Namecheap</TableCell>
-              <TableCell>
-                <Button size="xs" variant="primary" color="indigo">
-                  See details
-                </Button>
-              </TableCell>
-              <TableCell>
-                <Flex justifyContent="end" className="space-x-2">
-                  <button>
-                    <Icon
-                      icon={Pencil}
-                      variant="light"
-                      size="md"
-                      color="indigo"
-                    />
-                  </button>
-                  <button>
-                    <Icon icon={Trash} variant="light" size="md" color="red" />
-                  </button>
-                </Flex>
-              </TableCell>
-            </TableRow>
+            {data.domains.map(({ domainName }) => (
+              <TableRow key={domainName}>
+                <TableCell>{domainName}</TableCell>
+                <TableCell>05/04/2024</TableCell>
+                <TableCell>05/04/2023</TableCell>
+                <TableCell>12/04/2023</TableCell>
+                <TableCell>
+                  <Badge color="green" size="xs">
+                    <span className="font-medium">Active</span>
+                  </Badge>
+                </TableCell>
+                <TableCell>Namecheap</TableCell>
+                <TableCell>
+                  <Button size="xs" variant="primary" color="indigo">
+                    See details
+                  </Button>
+                </TableCell>
+                <TableCell>
+                  <Flex justifyContent="end" className="space-x-2">
+                    <button>
+                      <Icon
+                        icon={Pencil}
+                        variant="light"
+                        size="md"
+                        color="indigo"
+                      />
+                    </button>
+                    <button>
+                      <Icon
+                        icon={Trash}
+                        variant="light"
+                        size="md"
+                        color="red"
+                      />
+                    </button>
+                  </Flex>
+                </TableCell>
+              </TableRow>
+            ))}
           </TableBody>
         </Table>
       </Card>
