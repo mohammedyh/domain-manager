c3d96c0d2f269d2876d3e08310e3f0e0f923a06c
mohammedyh <mohammedyousufhamid@gmail.com>
2023-06-21 23:51:44 +0100

    Create endpoint to return domains for current user

--- server/src/server.js
+++ server/src/server.js
@@ -1,14 +1,27 @@
-import { Prisma, PrismaClient } from "@prisma/client";
 import "dotenv/config";
 import express from "express";
+import { ClerkExpressRequireAuth } from "@clerk/clerk-sdk-node";
+import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+app.use(ClerkExpressRequireAuth());
 app.use(express.json());
 
-// const userList = await clerk.users.getUserList();
+app.get("/api/domains", async (req, res) => {
+  const domains = await prisma.domain.findMany({
+    where: { user: req.auth.userId },
+    select: { domainName: true },
+  });
+
+  if (!domains) {
+    return res.status(200).json({ message: "No domains found" });
+  }
+
+  return res.status(200).json({ domains, success: "true" });
+});
 
 app.post("/api/domains/add", async (req, res) => {
   const { domainName, userId } = req.body;
