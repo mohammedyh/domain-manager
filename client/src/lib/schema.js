9657ebcf384c29b64a0a952217f16858e0be6094
mohammedyh <mohammedyousufhamid@gmail.com>
2023-07-02 00:34:44 +0100

    Convert input value to lowercase in domain schema

--- client/src/lib/schema.js
+++ client/src/lib/schema.js
@@ -2,6 +2,7 @@ import { string } from "yup";
 
 export const domainSchema = string()
   .trim()
+  .lowercase()
   .min(3, "Domain must be least 3 characters")
   .matches(
     /(?=^.{4,253}$)(^((?!-)[a-zA-Z0-9-]{0,62}[a-zA-Z0-9]\.)+[a-zA-Z]{2,20}$)/g,
