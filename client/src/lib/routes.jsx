4d5da8d647d9dcd7ca02030aa3007ec1c0887a9a
mohammedyh <mohammedyousufhamid@gmail.com>
2023-07-09 00:08:29 +0100

    Separate client-side routes into own folder for better organization

---
+++ client/src/lib/routes.jsx
@@ -0,0 +1,37 @@
+import { SignIn, SignUp, SignedIn, SignedOut } from "@clerk/clerk-react";
+import { Navigate } from "react-router-dom";
+import App from "../App";
+
+const routes = [
+  {
+    path: "/",
+    element: (
+      <>
+        <SignedIn>
+          <App />
+        </SignedIn>
+        <SignedOut>
+          <Navigate to="/signin" />
+        </SignedOut>
+      </>
+    ),
+  },
+  {
+    path: "/signin/",
+    element: (
+      <div className="grid place-content-center min-h-screen">
+        <SignIn signUpUrl="/signup" />
+      </div>
+    ),
+  },
+  {
+    path: "/signup/",
+    element: (
+      <div className="grid place-content-center min-h-screen">
+        <SignUp signInUrl="/signin" />
+      </div>
+    ),
+  },
+];
+
+export default routes;
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,20 +1,10 @@
-import {
-  ClerkProvider,
-  SignIn,
-  SignUp,
-  SignedIn,
-  SignedOut,
-} from "@clerk/clerk-react";
+import { ClerkProvider } from "@clerk/clerk-react";
 import "@fontsource-variable/inter";
 import React from "react";
 import ReactDOM from "react-dom/client";
-import {
-  Navigate,
-  RouterProvider,
-  createBrowserRouter,
-} from "react-router-dom";
-import App from "./App";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import "./index.css";
+import routes from "./lib/routes";
 
 const clerkPubKey = import.meta.env.VITE_REACT_APP_CLERK_PUBLISHABLE_KEY;
 
@@ -22,37 +12,7 @@ if (!clerkPubKey) {
   throw new Error("Missing publishable key");
 }
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: (
-      <>
-        <SignedIn>
-          <App />
-        </SignedIn>
-        <SignedOut>
-          <Navigate to="/signin" />
-        </SignedOut>
-      </>
-    ),
-  },
-  {
-    path: "/signin/",
-    element: (
-      <div className="grid place-content-center min-h-screen">
-        <SignIn signUpUrl="/signup" />
-      </div>
-    ),
-  },
-  {
-    path: "/signup/",
-    element: (
-      <div className="grid place-content-center min-h-screen">
-        <SignUp signInUrl="/signin" />
-      </div>
-    ),
-  },
-]);
+const router = createBrowserRouter(routes);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
