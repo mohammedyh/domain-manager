066f7a92de83d3a23994176fee6f273bb55b2e43
mohammedyh <mohammedyousufhamid@gmail.com>
2023-07-09 16:26:33 +0100

    Fix add domain button styling on mobile

--- client/src/components/Button.jsx
+++ client/src/components/Button.jsx
@@ -3,7 +3,7 @@ import { twMerge } from "tailwind-merge";
 
 export default function Button({ children, className, ...props }) {
   const buttonClasses = twMerge(
-    "rounded-md bg-indigo-600 px-4 py-2 text-xs sm:text-sm font-medium text-white hover:bg-opacity-80 transition-colors focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-400 focus-visible:ring-opacity-75",
+    "rounded-md bg-indigo-600 px-4 py-2 text-sm font-medium text-white hover:bg-opacity-80 transition-colors focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-400 focus-visible:ring-opacity-75",
     className
   );
 
