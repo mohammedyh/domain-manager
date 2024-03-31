e9f2831b63250288039437e78e2decd1b8070826
mohammedyh <mohammedyousufhamid@gmail.com>
2024-03-31 19:17:26 +0100

    Add namespace to import and replace __dirname in vite config

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,13 +1,12 @@
-import path from 'path'
-import { defineConfig } from 'vite'
+import path from 'node:path'
 import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      "@": path.resolve(import.meta.dirname, "./src"),
     },
   },
   server: {
