53e9cf05013437a92d3d35a0c227b11163c46171
mohammedyh <mohammedyousufhamid@gmail.com>
2024-10-28 11:40:11 +0000

    feat: upgrade to eslint 9 - flat config

---
+++ client/eslint.config.js
@@ -0,0 +1,42 @@
+import eslint from "@eslint/js";
+import reactPlugin from "eslint-plugin-react";
+import reactHooksPlugin from "eslint-plugin-react-hooks";
+import reactRefreshPlugin from "eslint-plugin-react-refresh";
+import globals from "globals";
+
+export default [
+  eslint.configs.recommended,
+  {
+    files: ["**/*.{js,jsx,mjs,cjs,ts,tsx}"],
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+      globals: {
+        ...globals.browser,
+        ...globals.es2020,
+      },
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+    },
+    plugins: {
+      react: reactPlugin,
+      "react-hooks": reactHooksPlugin,
+      "react-refresh": reactRefreshPlugin,
+    },
+    rules: {
+      ...reactPlugin.configs.recommended.rules,
+      ...reactPlugin.configs["jsx-runtime"].rules,
+      ...reactHooksPlugin.configs.recommended.rules,
+      "react-refresh/only-export-components": "warn",
+      "react/prop-types": 0,
+    },
+    settings: {
+      react: {
+        version: "18.3",
+      },
+    },
+  },
+];
