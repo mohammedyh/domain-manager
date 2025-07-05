cf038de465018c8c13c3097791a5934a2f495135
mohammedyh <32526267+mohammedyh@users.noreply.github.com>
2025-07-05 22:45:04 +0000

    style(ci): fix formatting

--- client/src/components/ui/button.jsx
+++ client/src/components/ui/button.jsx
@@ -40,7 +40,10 @@ const Button = React.forwardRef(
     const Comp = asChild ? Slot : "button";
     return (
       <Comp
-        className={cn(buttonVariants({ variant, size, className }), "cursor-pointer")}
+        className={cn(
+          buttonVariants({ variant, size, className }),
+          "cursor-pointer"
+        )}
         ref={ref}
         {...props}
       />
